ad7f9f14789dfc766fdadcb051c1318f9bc7791e
niciki <71276784+niciki@users.noreply.github.com>
2021-10-13 11:11:38 +0300

    Update TNaryTree.cpp

--- lab2/TNaryTree.cpp
+++ lab2/TNaryTree.cpp
@@ -1,5 +1,6 @@
 #include "TNaryTree.h"
 #include <string>
+#include <stdexcept>
 
 TNaryTree::TNaryTree(int n) {
     max_number = n;
@@ -13,32 +14,32 @@ bool TNaryTree::Empty(){
 
 void TNaryTree::Update(Rectangle &&polygon, std::string &&tree_path){
     if(tree_path != "" && curr_number == 0){
-        std::cout << "то функция должна бросить исключение std::invalid_argument";
+        throw std::invalid_argument("Error, there is not a root value\n");
         return;
     } else if(tree_path == "" && curr_number == 0){
         Item* q = new Item(polygon);
         root = q; 
         curr_number++;
     } else if(curr_number + 1 > max_number){
-        std::cout << "то функция должна бросить исключение std::out_of_range";
+        throw std::out_of_range("Current number of elements equals maximal number of elements in tree\n");
         return;
     } else {
         Item* tmp = root;
         for(int i = 0; i < tree_path.length() - 1; i++){
             if(tree_path[i] == 'b'){
                 if(tmp->bro == nullptr){
-                    std::cout << "то функция должна бросить исключение std::invalid_argument";
+                    throw std::invalid_argument("Path does not exist\n");
                     return;
                 }
                 tmp = tmp->bro;
             } else if(tree_path[i] == 'c'){
                 if(tmp->son == nullptr){
-                    std::cout << "то функция должна бросить исключение std::invalid_argument";
+                    throw std::invalid_argument("Path does not exist\n");
                     return;
                 }
                 tmp = tmp->son;
             } else {
-                std::cout << "то функция должна бросить исключение std::invalid_argument";
+                throw std::invalid_argument("Error in path\n");
                 return;
             }
         }
@@ -50,7 +51,7 @@ void TNaryTree::Update(Rectangle &&polygon, std::string &&tree_path){
             tmp->son = item;
             this->curr_number++;
         } else {
-            std::cout << "то функция должна бросить исключение std::invalid_argument";
+            throw std::invalid_argument("Error in path\n");
             return;
         }
     }
@@ -69,7 +70,6 @@ Item* copy(Item* root){
 TNaryTree::TNaryTree(const TNaryTree& other){
     curr_number = 0;
     max_number = other.max_number;
-    std::cout <<"Areas:\n";
     root = copy(other.root);
     curr_number = other.curr_number;
 ;}
@@ -99,20 +99,20 @@ void TNaryTree::Clear(std::string &&tree_path){
     for(int i = 0; i < tree_path.length(); i++){
         if(tree_path[i] == 'b'){
             if(tmp->bro == nullptr){
-                std::cout << "то функция должна бросить исключение std::invalid_argument";
+                throw std::invalid_argument("Path does not exist\n");
                 return;
             }
             prev_tmp = tmp;
             tmp = tmp->bro;
         } else if(tree_path[i] == 'c'){
             if(tmp->son == nullptr){
-                std::cout << "то функция должна бросить исключение std::invalid_argument";
+                throw std::invalid_argument("Path does not exist\n");
                 return;
             }
             prev_tmp = tmp;
             tmp = tmp->son;
         } else {
-            std::cout << "то функция должна бросить исключение std::invalid_argument";
+            throw std::invalid_argument("Error in path\n");
             return;
         }
     }
@@ -136,18 +136,18 @@ double TNaryTree::Area(std::string &&tree_path){
     for(int i = 0; i < tree_path.length(); i++){
         if(tree_path[i] == 'b'){
             if(tmp->bro == nullptr){
-                std::cout << "то функция должна бросить исключение std::invalid_argument";
+                throw std::invalid_argument("Path does not exist\n");
                 return -1;
             }
             tmp = tmp->bro;
         } else if(tree_path[i] == 'c'){
             if(tmp->son == nullptr){
-                std::cout << "то функция должна бросить исключение std::invalid_argument";
+                throw std::invalid_argument("Path does not exist\n");
                 return -1;
             }
             tmp = tmp->son;
         } else {
-            std::cout << "то функция должна бросить исключение std::invalid_argument";
+            throw std::invalid_argument("Error in path\n");
             return -1;
         }
     }
@@ -162,21 +162,29 @@ void print(std::ostream& os, Item* node){
         return;
     }
     if(node->son){
+        //os <<  <<node->pentagon.GetArea() <<  : ]" << 
         os << node->Area() << ": [";
         print(os, node->son);
-        os << "]";
         if(node->bro){
             if(node->bro){
                 os << ", ";
                 print(os, node->bro);
             }
         }
-    } else {
-        os << node->Area();
-        if(node->bro){
-            os << ", ";
-            print(os, node->bro);
+        os << "]";
+    } else if (node->bro) {
+       os << node->Area() << ": [";
+        print(os, node->bro);
+        if(node->son){
+            if(node->son){
+                os << ", ";
+                print(os, node->son);
+            }
         }
+        os << "]";
+    }
+    else {
+        os << node->Area();
     }
 }
 
