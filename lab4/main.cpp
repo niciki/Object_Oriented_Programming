4ce1cdb493030a3197b480b88548bdebda692dc6
niciki <71276784+niciki@users.noreply.github.com>
2021-10-20 18:06:30 +0300

    Create main.cpp

---
+++ lab4/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "TNaryTree.h"
+
+int main(){
+    TNaryTree<Rectangle> t(5);
+    t.Update(Rectangle(Point(0, 0), Point(1, 0),Point(1, 2), Point(0, 2)), "");
+    t.Update(Rectangle(Point(0, 0), Point(4, 0),Point(4, 1), Point(0, 1)), "b");
+    t.Update(Rectangle(Point(0, 0), Point(4, 0),Point(4, 1), Point(0, 1)), "bb");
+    t.Update(Rectangle(Point(0, 0), Point(4, 0),Point(4, 1), Point(0, 1)), "bbc");
+    t.Update(Rectangle(Point(0, 0), Point(4, 0),Point(4, 1), Point(0, 1)), "c");
+    std::cout << t.size() << "\n";
+    std::cout << t.Area("") << "\n";
+    std::cout << t.size() << "\n";
+    std::cout << t;
+    TNaryTree<Rectangle> q(t);
+    t.Clear();
+    std::cout << q.size() << " " << q.Area("") << "\n";
+    std::cout << q;
+}
